a881352cdd3ac8955b53fb407f1ee6f82fe809ea
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2025-01-11 12:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-// complete this js code
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
+  this.name = name;
+  this.age = age;
 }
 
-Person.prototype.greet = function () {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`)
-}
-
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+};
 
 function Employee(name, age, jobTitle) {
-	Person.call(this, name , age);
-	this.jobTitle = jobTitle;
+  // Call Person's constructor to initialize name and age
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
 
+// Set Employee's prototype to be an object created from Person's prototype
 Employee.prototype = Object.create(Person.prototype);
+// Ensure the constructor property points back to Employee
 Employee.prototype.constructor = Employee;
 
-Employee.prototype.jobGreet = function () {
-	console.log(
-     `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`
-   );
-}
+Employee.prototype.jobGreet = function() {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`
+  );
+};
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
